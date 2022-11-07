9ac6f6b8380e1ab60a68a82ed5e978213032244a
oggiV <491216@student.fontys.nl>
2022-11-07 09:40:10 +0100

    bruh

--- io-collega-tracker-frontend/src/pages/VerifyWait.tsx
+++ io-collega-tracker-frontend/src/pages/VerifyWait.tsx
@@ -6,15 +6,15 @@ const VerifyWait: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <form>
+        <div className='content'>
           <h1>A verification mail has been sent to this email address:</h1> <br/>
-          <label className="mail">example@iodigital.com</label>
+          <label className="mail">example@iodigital.com</label> <br />
           <i className="fa-solid fa-envelope-open-text fa-5x"></i>
           <div className='submit'>
-            <label className="label">Please verify your entered mail address by clicking on the link in the sent verification mail.</label>
+            <label className="label">Please verify your entered mail address by clicking on the link in the sent verification mail.</label> <br />
             <button className="btn">Resend Email</button>
           </div>
-        </form>
+        </div>
       </IonContent>
     </IonPage>
   );
