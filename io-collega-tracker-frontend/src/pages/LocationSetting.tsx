c71e39960a39190735bebbc4f537e61fcef26dac
oggiV <491216@student.fontys.nl>
2022-11-07 10:21:38 +0100

    StijlUpdate

--- io-collega-tracker-frontend/src/pages/LocationSetting.tsx
+++ io-collega-tracker-frontend/src/pages/LocationSetting.tsx
@@ -3,9 +3,12 @@ import ExploreContainer from '../components/ExploreContainer';
 import "./LocationSetting.css"
 
 const LocationSetting: React.FC = () => {
-    function ToggleLocation() {
-        (document.getElementById("yesbtn") as HTMLButtonElement).disabled = !(document.getElementById("yesbtn") as HTMLButtonElement).disabled;
-        (document.getElementById("nobtn") as HTMLButtonElement).disabled = !(document.getElementById("nobtn") as HTMLButtonElement).disabled;
+    function SetNo() {
+        
+    }
+
+    function SetYes() {
+
     }
 
     return (
@@ -15,8 +18,8 @@ const LocationSetting: React.FC = () => {
                     <h1>Set your location:</h1> <br />
                     <div className='blockcontainer'>
                         <h4>Are you at the office today?</h4>
-                        <button id="yesbtn" className='togglebtn' onClick={ToggleLocation}>Yes</button>
-                        <button id="nobtn" className='togglebtn' disabled onClick={ToggleLocation}>No</button>
+                        <button id="yesbtn" className='togglebtn' onClick={SetYes}>Yes</button>
+                        <button id="nobtn" className='togglebtn' onClick={SetNo}>No</button>
                     </div>
                 </div>
             </IonContent>
