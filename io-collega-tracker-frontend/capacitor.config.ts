f275468aeafd7e434cc393e3ef60d93672debd6b
Josian van Efferen <github@josian.nl>
2022-11-22 22:14:41 +0100

    Fixed broken app

--- io-collega-tracker-frontend/capacitor.config.ts
+++ io-collega-tracker-frontend/capacitor.config.ts
@@ -1,15 +1,15 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.colleaguetracker',
   appName: 'IO Collega Tracker Frontend',
   webDir: 'build',
   bundledWebRuntime: false,
   plugins: {
     GoogleAuth: {
       scopes: ["profile", "email"],
-      serverClientId: "733509514183-sa302u6f1fhqc7a93j789daqmgr63ckv.apps.googleusercontent.com",
-      androidClientId: "733509514183-sa302u6f1fhqc7a93j789daqmgr63ckv.apps.googleusercontent.com",
+      serverClientId: "438901753170-4s3sdluu5ib01dovth3sva1572ecpvkr.apps.googleusercontent.com",
+      androidClientId: "438901753170-4s3sdluu5ib01dovth3sva1572ecpvkr.apps.googleusercontent.com",
       iosClientId: "733509514183-vs2j883s5mjmg410v4v24ddiqu3slkd6.apps.googleusercontent.com",
       forceCodeForRefreshToken: true,
     }
